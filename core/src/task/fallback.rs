5bcbbe80213b84e4afaacd6d1685f38226b6384e
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-09 20:28:12 +0300

    Changed fallback task to be a fallback frame to compliment the new task system

--- core/src/task/fallback.rs
+++ core/src/task/fallback.rs
@@ -1,43 +1,7 @@
-use std::error::Error;
-use std::num::NonZeroU64;
+use std::fmt::Debug;
 use std::sync::Arc;
-use arc_swap::ArcSwap;
 use async_trait::async_trait;
-use chrono::{DateTime, Local};
-use typed_builder::TypedBuilder;
-use crate::overlap::{OverlapStrategy, SequentialOverlapPolicy};
-use crate::task::{Schedule, Task};
-
-#[derive(TypedBuilder)]
-#[builder(build_method(into = FallbackTask<T, T2>))]
-pub struct FallbackTaskConfig<T, T2>
-where
-    T: Task,
-    T2: Task,
-    FallbackTask<T, T2>: From<FallbackTaskConfig<T, T2>>,
-{
-    primary: T,
-    fallback_task: T2,
-
-    #[builder(default = Arc::new(SequentialOverlapPolicy))]
-    overlap_policy: Arc<dyn OverlapStrategy>,
-
-    #[builder(default, setter(strip_option))]
-    max_runs: Option<NonZeroU64>,
-}
-
-impl<T: Task, T2: Task> From<FallbackTaskConfig<T, T2>> for FallbackTask<T, T2> {
-    fn from(config: FallbackTaskConfig<T, T2>) -> Self {
-        let creation_time = Local::now();
-        Self {
-            task: config.primary,
-            fallback: config.fallback_task,
-            max_runs: config.max_runs,
-            last_execution: ArcSwap::from_pointee(creation_time),
-            overlap_policy: config.overlap_policy,
-        }
-    }
-}
+use crate::task::{TaskFrame, TaskMetadata};
 
 /// Represents a **fallback task** which wraps two other tasks. This task type acts as a
 /// **composite node** within the task hierarchy, providing a failover mechanism for execution.
@@ -78,62 +42,32 @@ impl<T: Task, T2: Task> From<FallbackTaskConfig<T, T2>> for FallbackTask<T, T2> 
 ///
 /// CHRONOLOG_SCHEDULER.register(fallback_task).await;
 /// ```
-pub struct FallbackTask<T: Task, T2: Task> {
-    task: T,
-    fallback: T2,
-    last_execution: ArcSwap<DateTime<Local>>,
-    max_runs: Option<NonZeroU64>,
-    overlap_policy: Arc<dyn OverlapStrategy>,
-}
+pub struct FallbackTask<T: 'static, T2: 'static>(T, T2);
 
-impl<T: Task, T2: Task> FallbackTask<T, T2> {
-    pub fn builder() -> FallbackTaskConfigBuilder<T, T2> {
-        FallbackTaskConfig::builder()
+impl<T, T2> FallbackTask<T, T2>
+where
+    T: TaskFrame + 'static,
+    T2: TaskFrame + 'static,
+{
+    pub fn new(primary: T, secondary: T2) -> Self {
+        Self(primary, secondary)
     }
 }
 
 #[async_trait]
-impl<T: Task, T2: Task> Task for FallbackTask<T, T2> {
-    async fn execute_inner(&self) -> Result<(), Arc<dyn Error + Send + Sync>> {
-        let result = match self.task.execute_inner().await {
-            Err(_) => self.fallback.execute_inner().await,
+impl<T, T2> TaskFrame for FallbackTask<T, T2>
+where
+    T: TaskFrame + 'static,
+    T2: TaskFrame + 'static,
+{
+    async fn execute(&self, metadata: &(dyn TaskMetadata + Send + Sync)) -> Result<(), Arc<dyn Debug + Send + Sync>> {
+        let result = match self.0.execute(metadata).await {
+            Err(_) => {
+                let result = self.1.execute(metadata).await;
+                result
+            },
             res => res
         };
-        self.last_execution.store(Arc::new(Local::now()));
         result
-    }
-
-    async fn get_schedule(&self) -> Arc<dyn Schedule> {
-        self.task.get_schedule().await
-    }
-
-    async fn total_runs(&self) -> u64 {
-        self.task.total_runs().await
-    }
-
-    async fn maximum_runs(&self) -> Option<NonZeroU64> {
-        self.max_runs
-    }
-
-    async fn set_maximum_runs(&mut self, max_runs: NonZeroU64) {
-        self.max_runs = Some(max_runs)
-    }
-
-    async fn set_total_runs(&mut self, _runs: u64) {}
-
-    async fn set_last_execution(&mut self, exec: DateTime<Local>) {
-        self.last_execution.swap(Arc::new(exec));
-    }
-
-    async fn get_debug_label(&self) -> String {
-        self.task.get_debug_label().await
-    }
-
-    async fn last_execution(&self) -> DateTime<Local> {
-        *self.last_execution.load().clone()
-    }
-
-    async fn overlap_policy(&self) -> Arc<dyn OverlapStrategy> {
-        self.overlap_policy.clone()
     }
 }
