09df5df03c7a371f8fdfba9a88176d1e57dcce04
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-09 20:25:40 +0300

    Added an abort error and changed FailedExecution to have an Arc of debuggable as opposed to a box debuggable

--- core/src/errors.rs
+++ core/src/errors.rs
@@ -1,10 +1,14 @@
 use std::fmt::{Debug};
+use std::sync::Arc;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ChronologErrors {
     #[error("`{0}` Failed to successfully execute, the function returned an error: {1:?}")]
-    FailedExecution(String, Box<dyn Debug + Send + Sync>),
+    FailedExecution(String, Arc<dyn Debug + Send + Sync>),
+
+    #[error("`{0}` was aborted")]
+    TaskAborted(String),
 
     #[error("`{0}` Timed out")]
     TimeoutError(String)
