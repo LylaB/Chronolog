2f93498f2bfcdff55514f3fd725062247cf7c60c
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-09 20:27:57 +0300

    Changed retry task to be a retry frame to compliment the new task system

--- core/src/task/retry.rs
+++ core/src/task/retry.rs
@@ -1,37 +1,9 @@
-use std::error::Error;
-use std::num::{NonZeroU32, NonZeroU64};
+use std::fmt::Debug;
+use std::num::{NonZeroU32};
 use std::sync::Arc;
 use std::time::Duration;
-use arc_swap::ArcSwap;
 use async_trait::async_trait;
-use chrono::{DateTime, Local};
-use typed_builder::TypedBuilder;
-use crate::overlap::OverlapStrategy;
-use crate::task::{Schedule, Task};
-
-#[derive(TypedBuilder)]
-#[builder(build_method(into = RetriableTask<T>))]
-pub struct RetriableTaskConfig<T>
-where
-    T: Task,
-    RetriableTask<T>: From<RetriableTaskConfig<T>>,
-{
-    task: T,
-    retries: NonZeroU32,
-    delay: Duration
-}
-
-impl<T: Task> From<RetriableTaskConfig<T>> for RetriableTask<T> {
-    fn from(config: RetriableTaskConfig<T>) -> Self {
-        let creation_time = Local::now();
-        Self {
-            task: config.task,
-            retries: config.retries,
-            delay: config.delay,
-            last_execution: ArcSwap::from_pointee(creation_time),
-        }
-    }
-}
+use crate::task::{TaskFrame, TaskMetadata};
 
 /// Represents a **retriable task** which wraps a task. This task type acts as a
 /// **wrapper node** within the task hierarchy, providing a retry mechanism for execution.
@@ -65,71 +37,38 @@ impl<T: Task> From<RetriableTaskConfig<T>> for RetriableTask<T> {
 ///
 /// CHRONOLOG_SCHEDULER.register(retriable_task).await;
 /// ```
-pub struct RetriableTask<T: Task> {
+pub struct RetriableTask<T: 'static> {
     task: T,
     retries: NonZeroU32,
     delay: Duration,
-    last_execution: ArcSwap<DateTime<Local>>
 }
 
-impl<T: Task> RetriableTask<T> {
-    pub fn builder() -> RetriableTaskConfigBuilder<T> {
-        RetriableTaskConfig::builder()
+impl<T: TaskFrame + 'static> RetriableTask<T> {
+    pub fn new(task: T, retries: NonZeroU32, delay: Duration) -> Self {
+        Self {task, retries, delay}
+    }
+
+    pub fn new_instant(task: T, retries: NonZeroU32) -> Self {
+        Self {task, retries, delay: Duration::ZERO}
     }
 }
 
 #[async_trait]
-impl<T: Task> Task for RetriableTask<T> {
-    async fn execute_inner(&self) -> Result<(), Arc<dyn Error + Send + Sync>> {
-        let mut error: Option<Arc<dyn Error + Send + Sync>> = None;
+impl<T: TaskFrame + 'static> TaskFrame for RetriableTask<T> {
+    async fn execute(&self, metadata: &(dyn TaskMetadata + Send + Sync)) -> Result<(), Arc<dyn Debug + Send + Sync>> {
+        let mut error: Option<Arc<dyn Debug + Send + Sync>> = None;
         for _ in 0..self.retries.get() {
-            let result = self.task.execute_inner().await;
+            let result = self.task.execute(metadata).await;
             match result {
                 Ok(_) => {
-                    self.last_execution.store(Arc::new(Local::now()));
                     return Ok(())
                 },
-                Err(err) => error = Some(err)
+                Err(err) => {
+                    error = Some(err.clone());
+                }
             }
             tokio::time::sleep(self.delay).await;
         }
-        self.last_execution.store(Arc::new(Local::now()));
         Err(error.unwrap())
-    }
-
-    async fn get_schedule(&self) -> Arc<dyn Schedule> {
-        self.task.get_schedule().await
-    }
-
-    async fn total_runs(&self) -> u64 {
-        self.task.total_runs().await
-    }
-
-    async fn maximum_runs(&self) -> Option<NonZeroU64> {
-        self.task.maximum_runs().await
-    }
-
-    async fn set_maximum_runs(&mut self, max_runs: NonZeroU64) {
-        self.task.set_maximum_runs(max_runs).await;
-    }
-
-    async fn set_total_runs(&mut self, runs: u64) {
-        self.task.set_total_runs(runs).await;
-    }
-
-    async fn set_last_execution(&mut self, exec: DateTime<Local>) {
-        self.last_execution.swap(Arc::new(exec));
-    }
-
-    async fn get_debug_label(&self) -> String {
-        self.task.get_debug_label().await
-    }
-
-    async fn last_execution(&self) -> DateTime<Local> {
-        *self.last_execution.load().clone()
-    }
-
-    async fn overlap_policy(&self) -> Arc<dyn OverlapStrategy> {
-        self.task.overlap_policy().await
     }
 }
