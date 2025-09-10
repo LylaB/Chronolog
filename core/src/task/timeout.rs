2e1329a42f583a0397a25f7cc19b9997b737d4f2
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-10 17:33:57 +0300

    Rewrote the documentation for TimeoutTaskFrame, added events for start, end, on_timeout and tweaked the arguments as well as the methods to match the TaskFrame trait

--- core/src/task/timeout.rs
+++ core/src/task/timeout.rs
@@ -2,60 +2,98 @@ use std::fmt::Debug;
 use std::sync::Arc;
 use std::time::Duration;
 use async_trait::async_trait;
-use crate::task::{TaskFrame, TaskMetadata};
+use crate::task::{ArcTaskEvent, ExposedTaskMetadata, TaskEndEvent, TaskError, TaskEvent, TaskEventEmitter, TaskFrame, TaskStartEvent};
 
-/// Represents a **timeout task** which wraps a task. This task type acts as a
-/// **wrapper node** within the task hierarchy, providing a timeout mechanism for execution.
+/// Represents a **timeout task frame** which wraps a task frame. This task frame type acts as a
+/// **wrapper node** within the task frame hierarchy, providing a timeout mechanism for execution.
 ///
-/// ### Behavior
-/// - Executes the **wrapped task**.
-/// - Tracks a timer while the task executes.
+/// # Behavior
+/// - Executes the **wrapped task frame**.
+/// - Tracks a timer while the task frame executes.
 /// - If the task executes longer than a specified duration, an error 
 /// is thrown and the task is aborted
+///
+/// # ⚠ Important Note ⚠
+/// Due to a limitation, if the task frame executes CPU-Bound logic mostly and does not yield,
+/// then the task frame may be completed, as such ensure the task frame has defined a sufficient
+/// number of cancellation points / yields
+///
+/// # Events
+/// [`TimeoutTaskFrame`] defines one single event, and that is `on_timeout`, it executes when the
+/// task frame is executing longer than the maximum duration allowed, it exposes no form of payload
 ///
 /// # Example
 /// ```ignore
 /// use std::time::Duration;
-/// use chronolog::schedule::ScheduleInterval;
-/// use chronolog::scheduler::{Scheduler, CHRONOLOG_SCHEDULER};
-/// use chronolog::task::fallback::FallbackTask;
-/// use chronolog::task::execution::ExecutionTask;
+/// use tokio::time::sleep;
+/// use chronolog_core::schedule::TaskScheduleInterval;
+/// use chronolog_core::scheduler::{Scheduler, CHRONOLOG_SCHEDULER};
+/// use chronolog_core::task::timeout::TimeoutTaskFrame;
+/// use chronolog_core::task::execution::ExecutionTaskFrame;
+/// use chronolog_core::task::Task;
 ///
-/// let exec_task = ExecutionTask::builder()
-///     .schedule(ScheduleInterval::duration(Duration::from_secs(2)))
-///     .func(|_metadata| async {
+/// let exec_frame = ExecutionTaskFrame::new(
+///     |_metadata| async {
 ///         println!("Trying primary task...");
+///         sleep(Duration::from_secs_f64(3.5)).await; // Suppose complex operations
 ///         Err::<(), ()>(())
-///     })
-///     .build();
+///     }
+/// );
 ///
-/// let retriable_task = TimeoutTask::builder()
-///     .task(exec_task)
-///     .max_duration(Duration::from_secs(3))
-///     .build();
+/// let timeout_frame = TimeoutTaskFrame::new(
+///     exec_frame,
+///     Duration::from_secs(3)
+/// );
 ///
-/// CHRONOLOG_SCHEDULER.register(retriable_task).await;
+/// let task = Task::define(TaskScheduleInterval::from_secs(4), timeout_frame);
+/// CHRONOLOG_SCHEDULER.register(task).await;
 /// ```
-pub struct TimeoutTask<T: 'static>(T, Duration);
+pub struct TimeoutTaskFrame<T: 'static> {
+    task: T,
+    max_duration: Duration,
+    on_start: TaskStartEvent,
+    on_end: TaskEndEvent,
+    pub on_timeout: ArcTaskEvent<()>
+}
 
-impl<T: TaskFrame + 'static> TimeoutTask<T> {
+impl<T: TaskFrame + 'static> TimeoutTaskFrame<T> {
     pub fn new(task: T, max_duration: Duration) -> Self {
-        TimeoutTask(task, max_duration)
+        TimeoutTaskFrame {
+            task,
+            max_duration,
+            on_start: TaskEvent::new(),
+            on_end: TaskEvent::new(),
+            on_timeout: TaskEvent::new(),
+        }
     }
 }
 
 #[async_trait]
-impl<T: TaskFrame + 'static> TaskFrame for TimeoutTask<T> {
-    async fn execute(&self, metadata: &(dyn TaskMetadata + Send + Sync)) -> Result<(), Arc<dyn Debug + Send + Sync>> {
+impl<T: TaskFrame + 'static> TaskFrame for TimeoutTaskFrame<T> {
+    async fn execute(
+        &self,
+        metadata: Arc<dyn ExposedTaskMetadata + Send + Sync>,
+        emitter: Arc<TaskEventEmitter>
+    ) -> Result<(), TaskError> {
         let result = tokio::time::timeout(
-            self.1, self.0.execute(metadata)
+            self.max_duration, self.task.execute(metadata.clone(), emitter.clone())
         ).await;
 
-        result.unwrap_or_else(|_| {
-            Err(Arc::new(std::io::Error::new(
-                std::io::ErrorKind::TimedOut,
-                "Task timed out",
-            )))
-        })
+        if let Ok(inner) = result {
+            return inner;
+        }
+        emitter.emit(metadata.clone(), self.on_timeout.clone(), ()).await;
+        Err(Arc::new(std::io::Error::new(
+            std::io::ErrorKind::TimedOut,
+            "Task timed out",
+        )))
+    }
+
+    fn on_start(&self) -> TaskStartEvent {
+        todo!()
+    }
+
+    fn on_end(&self) -> TaskEndEvent {
+        todo!()
     }
 }
