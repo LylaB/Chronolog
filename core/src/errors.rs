29802be0c326c0f2d19b4c45c412cf0d7603fb4f
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-10 17:17:42 +0300

    Used a type alias for TaskError instead of Arc<dyn Debug + Send + Sync>

--- core/src/errors.rs
+++ core/src/errors.rs
@@ -1,11 +1,11 @@
 use std::fmt::{Debug};
-use std::sync::Arc;
 use thiserror::Error;
+use crate::task::TaskError;
 
 #[derive(Error, Debug)]
 pub enum ChronologErrors {
     #[error("`{0}` Failed to successfully execute, the function returned an error: {1:?}")]
-    FailedExecution(String, Arc<dyn Debug + Send + Sync>),
+    FailedExecution(String, TaskError),
 
     #[error("`{0}` was aborted")]
     TaskAborted(String),
