309510ee4b51832713077afa5c30eba3a8910e77
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-10 17:31:25 +0300

    Rewrote the documentation for RetriableTaskFrame, added events for start, end, on_retry_start and on_retry_end and tweaked the arguments as well as the methods to match the TaskFrame trait

--- core/src/task/retry.rs
+++ core/src/task/retry.rs
@@ -1,74 +1,113 @@
+use crate::task::{ArcTaskEvent, ExposedTaskMetadata, TaskEndEvent, TaskError, TaskEvent, TaskStartEvent};
 use std::fmt::Debug;
 use std::num::{NonZeroU32};
 use std::sync::Arc;
 use std::time::Duration;
 use async_trait::async_trait;
-use crate::task::{TaskFrame, TaskMetadata};
+use crate::task::{TaskEventEmitter, TaskFrame};
 
-/// Represents a **retriable task** which wraps a task. This task type acts as a
-/// **wrapper node** within the task hierarchy, providing a retry mechanism for execution.
+/// Represents a **retriable task frame** which wraps a task frame. This task frame type acts as a
+/// **wrapper node** within the task frame hierarchy, providing a retry mechanism for execution.
 ///
-/// ### Behavior
-/// - Executes the **wrapped task**.
-/// - If the task fails, it re-executes it again after a specified delay (or instantaneous).
-/// - Repeat the process for a specified number of retries til the task succeeds
+/// # Behavior
+/// - Executes the **wrapped task frame**.
+/// - If the task frame fails, it re-executes it again after a specified delay (or instantaneous).
+/// - Repeat the process for a specified number of retries til the task frame succeeds
+///
+/// # Events
+/// [`RetriableTaskFrame`] provides 2 events, namely ``on_retry_start`` which executes when a retry
+/// happens, it hands out the wrapped task frame instance. As well as the ``on_retry_end`` which 
+/// executes when a retry is finished, it hands out the wrapped task frame instance and an option 
+/// error for a potential error it may have gotten from this retry
 ///
 /// # Example
 /// ```ignore
-/// use std::time::Duration;
-/// use chronolog::schedule::ScheduleInterval;
-/// use chronolog::scheduler::{Scheduler, CHRONOLOG_SCHEDULER};
-/// use chronolog::task::fallback::FallbackTask;
-/// use chronolog::task::execution::ExecutionTask;
+/// use std::num::NonZeroU32;
+/// use chronolog_core::schedule::TaskScheduleInterval;
+/// use chronolog_core::scheduler::{Scheduler, CHRONOLOG_SCHEDULER};
+/// use chronolog_core::task::retry::RetriableTaskFrame;
+/// use chronolog_core::task::execution::ExecutionTaskFrame;
+/// use chronolog_core::task::Task;
 ///
-/// let exec_task = ExecutionTask::builder()
-///     .schedule(ScheduleInterval::duration(Duration::from_secs(2)))
-///     .func(|_metadata| async {
+/// let exec_frame = ExecutionTaskFrame::new(
+///     |_metadata| async {
 ///         println!("Trying primary task...");
 ///         Err::<(), ()>(())
-///     })
-///     .build();
+///     }
+/// );
 ///
-/// let retriable_task = RetriableTask::builder()
-///     .task(exec_task)
-///     .retries(3)
-///     .delay(Duration::from_secs(0))
-///     .build();
+/// let retriable_frame = RetriableTaskFrame::new_instant(
+///     exec_frame,
+///     NonZeroU32::new(3).unwrap(), // We know it isn't zero, so safe to unwrap
+/// );
 ///
-/// CHRONOLOG_SCHEDULER.register(retriable_task).await;
+/// let task = Task::define(TaskScheduleInterval::from_secs_f64(2.5), retriable_frame);
+///
+/// CHRONOLOG_SCHEDULER.register(task).await;
 /// ```
-pub struct RetriableTask<T: 'static> {
-    task: T,
+pub struct RetriableTaskFrame<T: 'static> {
+    task: Arc<T>,
     retries: NonZeroU32,
     delay: Duration,
+    on_start: TaskStartEvent,
+    on_end: TaskEndEvent,
+    pub on_retry_start: ArcTaskEvent<Arc<T>>,
+    pub on_retry_end: ArcTaskEvent<(Arc<T>, Option<TaskError>)>,
 }
 
-impl<T: TaskFrame + 'static> RetriableTask<T> {
+impl<T: TaskFrame + 'static> RetriableTaskFrame<T> {
+    /// Creates a retriable task that has a specified delay per retry
     pub fn new(task: T, retries: NonZeroU32, delay: Duration) -> Self {
-        Self {task, retries, delay}
+        Self {
+            task: Arc::new(task), 
+            retries, 
+            delay,
+            on_start: TaskEvent::new(),
+            on_end: TaskEvent::new(),
+            on_retry_end: TaskEvent::new(),
+            on_retry_start: TaskEvent::new()
+        }
     }
 
+    /// Creates a retriable task that has no delay per retry
     pub fn new_instant(task: T, retries: NonZeroU32) -> Self {
-        Self {task, retries, delay: Duration::ZERO}
+        Self::new(task, retries, Duration::ZERO)
     }
 }
 
 #[async_trait]
-impl<T: TaskFrame + 'static> TaskFrame for RetriableTask<T> {
-    async fn execute(&self, metadata: &(dyn TaskMetadata + Send + Sync)) -> Result<(), Arc<dyn Debug + Send + Sync>> {
-        let mut error: Option<Arc<dyn Debug + Send + Sync>> = None;
-        for _ in 0..self.retries.get() {
-            let result = self.task.execute(metadata).await;
+impl<T: TaskFrame + 'static> TaskFrame for RetriableTaskFrame<T> {
+    async fn execute(
+        &self,
+        metadata: Arc<dyn ExposedTaskMetadata + Send + Sync>,
+        emitter: Arc<TaskEventEmitter>
+    ) -> Result<(), TaskError> {
+        let mut error: Option<TaskError> = None;
+        for i in 0..self.retries.get() {
+            if i != 0 {
+                emitter.emit(metadata.clone(), self.on_retry_start.clone(), self.task.clone()).await;
+            }
+            let result = self.task.execute(metadata.clone(), emitter.clone()).await;
             match result {
                 Ok(_) => {
+                    emitter.emit(metadata.clone(), self.on_retry_end.clone(), (self.task.clone(), None)).await;
                     return Ok(())
                 },
                 Err(err) => {
                     error = Some(err.clone());
+                    emitter.emit(metadata.clone(), self.on_retry_end.clone(), (self.task.clone(), error.clone())).await;
                 }
             }
             tokio::time::sleep(self.delay).await;
         }
         Err(error.unwrap())
+    }
+
+    fn on_start(&self) -> TaskStartEvent {
+        self.on_start.clone()
+    }
+
+    fn on_end(&self) -> TaskEndEvent {
+        self.on_end.clone()
     }
 }
