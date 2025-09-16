c7750a8ea0fdbdb42cf77c6cafaaede10e0590ae
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-16 11:48:52 +0300

    Fixed issues with cargo clippy and added a type alias for dynamic dispatched listeners (soon I will make it a trait itself)

--- core/src/task/events.rs
+++ core/src/task/events.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::sync::Arc;
+use async_trait::async_trait;
+use dashmap::DashMap;
 use tokio::sync::RwLock;
 use uuid::Uuid;
 use crate::task::metadata::ExposedTaskMetadata;
@@ -10,48 +12,50 @@ pub type TaskEndEvent = ArcTaskEvent<Option<TaskError>>;
 pub type ArcTaskEvent<P> = Arc<TaskEvent<P>>;
 pub type TaskError = Arc<dyn Debug + Send + Sync>;
 
+pub type DynListenerFunc<P> = Arc<dyn Fn(Arc<dyn ExposedTaskMetadata>, P) + Send + Sync>;
+
 /// [`TaskEvent`] defines an event which may (or may not, depending on how the frame implementation
 /// handles this task event) execute. This is the main system used for listening to various events,
 /// there are 2 types of events at play, which one can listen to:
 ///
 /// - **Lifecycle Task Events** These are automatically emitted by the scheduler, all task frames have this
-/// event no matter the type. Currently, there are 2 of these, the first namely being ``on_start``
-/// used for listening to when a task is about to start. While the second is ``on_end`` which is used
-/// for listening to when a task is ending (this event executes before the error handler)
+///   event no matter the type. Currently, there are 2 of these, the first namely being ``on_start``
+///   used for listening to when a task is about to start. While the second is ``on_end`` which is used
+///   for listening to when a task is ending (this event executes before the error handler)
 ///
 /// - **Local Task Events** These are local to the task frame, different task frames may have none, one
-/// or multiple of these event types. They are emitted by the task frame logic and give more extensibility
-/// to what outside parties can listen to (for example, on the fallback task frame, one can listen to
-/// when the fallback is about to execute)
+///   or multiple of these event types. They are emitted by the task frame logic and give more extensibility
+///   to what outside parties can listen to (for example, on the fallback task frame, one can listen to
+///   when the fallback is about to execute)
 ///
 /// [`TaskEvent`] **CANNOT** be emitted by itself, it needs an emitter which is only handed to the
 /// scheduler, overlapping policies and the task frame. Outside parties can listen to the event at any
 /// time they would like
 pub struct TaskEvent<P> {
-    listeners: RwLock<HashMap<Uuid, Arc<dyn Fn(Arc<dyn ExposedTaskMetadata>, &P) + Send + Sync>>>,
+    listeners: DashMap<Uuid, DynListenerFunc<P>>,
 }
 
 impl<P: Send + 'static> TaskEvent<P> {
     /// Creates a task event, containing no listeners
     pub fn new() -> Arc<Self> {
         Arc::new(Self {
-            listeners: RwLock::new(HashMap::new()),
+            listeners: DashMap::new(),
         })
     }
 
     /// Subscribes a listener to the task event, returning an identifier for that listener / subscriber
     pub async fn subscribe(
         &self,
-        func: impl Fn(Arc<dyn ExposedTaskMetadata>, &P) + Send + Sync + 'static,
+        func: impl Fn(Arc<dyn ExposedTaskMetadata>, P) + Send + Sync + 'static,
     ) -> Uuid {
         let id = Uuid::new_v4();
-        self.listeners.write().await.insert(id, Arc::new(func));
+        self.listeners.insert(id, Arc::new(func));
         id
     }
 
     /// Unsubscribes a listener to the task event, returning an identifier for that listener
     pub async fn unsubscribe(&self, id: Uuid) {
-        self.listeners.write().await.remove(&id);
+        self.listeners.remove(&id);
     }
 }
 
@@ -64,25 +68,21 @@ pub struct TaskEventEmitter {
 
 impl TaskEventEmitter {
     /// Emits the event, notifying all subscribers / listeners
-    pub async fn emit<P: Send + Sync>(
+    pub async fn emit<P: Send + Sync + Clone>(
         &self,
         metadata: Arc<dyn ExposedTaskMetadata>,
         event: Arc<TaskEvent<P>>,
         payload: P,
     ) {
-        let listeners = &*event.listeners.read().await;
-        match listeners.len() {
-            0 => {}
-            1 => listeners.values().next().unwrap()(metadata.clone(), &payload),
-            _ => {
-                std::thread::scope(|s| {
-                    for listener in listeners.values() {
-                        s.spawn(|| {
-                            listener(metadata.clone(), &payload);
-                        });
-                    }
+        std::thread::scope(|s| {
+            for listener in event.listeners.iter() {
+                let cloned_listener = listener.value().clone();
+                let cloned_metadata = metadata.clone();
+                let cloned_payload = payload.clone();
+                s.spawn(move || {
+                    cloned_listener(cloned_metadata, cloned_payload);
                 });
             }
-        };
+        });
     }
 }
