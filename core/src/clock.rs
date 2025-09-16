d20b4cf328e0fe5064b9d721459542e44e27d471
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-16 11:14:35 +0300

    Removed some useless methods for the clock trait

--- core/src/clock.rs
+++ core/src/clock.rs
@@ -6,13 +6,12 @@ pub use system_clock::SystemClock;
 
 use std::time::{SystemTime, Duration};
 use async_trait::async_trait;
-use chrono::{DateTime, OutOfRangeError, TimeDelta, TimeZone, Utc};
+use chrono::{DateTime, OutOfRangeError, TimeDelta, TimeZone};
 
 
 /// [`SchedulerClock`] is a trait for implementing a custom scheduler clock, typical operations
 /// include getting the current time, idle for a specific duration (or til a specific date is reached).
-/// There are also versions of these methods for converting via timezones or related duration/time types, 
-/// as it goes for implementations, by default there are 2 of them:
+/// As it goes for implementations, by default, there are 2 of them:
 ///
 /// - [`VirtualClock`] used to simulate time (for unit-tests, debugging,
 /// [`flashcrowd`](https://en.wiktionary.org/wiki/flashcrowd#English) simulations... etc.), it doesn't
@@ -36,48 +35,9 @@ pub trait SchedulerClock: Send + Sync {
     /// Gets the current time of the clock
     async fn now(&self) -> SystemTime;
 
-    /// Idle for a specified duration
-    async fn idle(&self, duration: Duration) {
-        self.idle_to(self.now().await + duration).await;
-    }
-
     /// Idle until this specified time is reached (if it is in the past or present, it doesn't idle)
     async fn idle_to(&self, to: SystemTime);
 }
-
-#[allow(unused)]
-trait SchedulerClockExt: SchedulerClock {
-    /// Gets the current time of the clock from a timezone
-    async fn now(&self, tz: &impl TimeZone) -> DateTime<impl TimeZone> {
-        let sys_time = SchedulerClock::now(self).await;
-        let datetime: DateTime<Utc> = DateTime::from(sys_time);
-        datetime.with_timezone(tz)
-    }
-
-    /// Idles for a specified time based on a `Into<Duration>`
-    async fn idle(&self, duration: impl Into<Duration>) {
-        SchedulerClock::idle(self, duration.into()).await;
-    }
-
-    /// Tries to idle based on a `TryInto<Duration>`, if the conversion fails, then it returns an error
-    async fn try_idle<T: TryInto<Duration>>(&self, duration: T) -> Result<(), T::Error> {
-        SchedulerClock::idle(self, duration.try_into()?).await;
-        Ok(())
-    }
-
-    /// Tries to idle based on a `TimeDelta`, if the conversion fails, then it returns an error
-    async fn try_idle_timedelta(&self, duration: TimeDelta) -> Result<(), OutOfRangeError> {
-        SchedulerClock::idle(self, duration.to_std()?).await;
-        Ok(())
-    }
-
-    /// Idles until this specified time from a timezone is reached (if it is in the past or present, it doesn't idle)
-    async fn idle_to(&self, to: DateTime<impl TimeZone>) {
-        SchedulerClock::idle_to(self, SystemTime::from(to)).await
-    }
-}
-
-impl<T: SchedulerClock + ?Sized> SchedulerClockExt for T {}
 
 /// [`AdvanceableScheduleClock`] is an optional extension to [`SchedulerClock`] which, as the name
 /// suggests, allows for arbitrary advancement of time via [`AdvanceableScheduleClock::advance`] or
