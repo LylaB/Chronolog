6cdc20d783eb4a45516049fe7c9c8139637c9534
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-16 11:22:19 +0300

    Added Task Priority as an enum

---
+++ core/src/task/priority.rs
@@ -0,0 +1,16 @@
+/// [`TaskPriority`] dictates the importance of a task, the more important a task is,
+/// the more Chronolog ensures to execute the task at a specific time without any latency,
+/// no matter what, the lower level of the spectrum is [`TaskPriority::LOW`] while the highest it
+/// can be is [`TaskPriority::CRITICAL`] where no single time drift is allowed
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
+pub enum TaskPriority {
+    CRITICAL,
+    IMPORTANT,
+    HIGH,
+
+    #[default]
+    MODERATE,
+
+    LOW
+}
