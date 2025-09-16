bc7d9e157e57f721263ce2aa7da222bc9f9a00ed
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-16 11:21:56 +0300

    Added some more utils for conversion between SystemTime and DateTime<Local>

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,3 +1,6 @@
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use chrono::{DateTime, Local, TimeZone};
+
 #[macro_export]
 macro_rules! policy_match {
     (
@@ -21,4 +24,29 @@ macro_rules! policy_match {
             }
         }
     }};
+}
+
+pub fn system_time_to_date_time(t: SystemTime) -> DateTime<Local> {
+    let (sec, nsec) = match t.duration_since(UNIX_EPOCH) {
+        Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
+        Err(e) => {
+            let dur = e.duration();
+            let (sec, nsec) = (dur.as_secs() as i64, dur.subsec_nanos());
+            if nsec == 0 {
+                (-sec, 0)
+            } else {
+                (-sec - 1, 1_000_000_000 - nsec)
+            }
+        },
+    };
+    Local.timestamp_opt(sec, nsec).unwrap()
+}
+
+pub fn date_time_to_system_time(dt: DateTime<Local>) -> SystemTime {
+    let duration_since_epoch = dt.timestamp_nanos_opt().unwrap();
+    if duration_since_epoch >= 0 {
+        UNIX_EPOCH + Duration::from_nanos(duration_since_epoch as u64)
+    } else {
+        UNIX_EPOCH - Duration::from_nanos((-duration_since_epoch) as u64)
+    }
 }
