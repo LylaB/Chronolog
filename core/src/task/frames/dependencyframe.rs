9aaa84d4d801e4ddf3c4c5ce28872a55224a0e9a
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-18 17:25:04 +0300

    Added DependencyTaskFrame for task frames that depend on other tasks

---
+++ core/src/task/frames/dependencyframe.rs
@@ -0,0 +1,150 @@
+use crate::errors::ChronologErrors;
+use crate::task::dependency::FrameDependency;
+use crate::task::{
+    Arc, TaskEndEvent, TaskError, TaskEvent, TaskEventEmitter, TaskFrame, TaskMetadata,
+    TaskStartEvent,
+};
+use async_trait::async_trait;
+use std::fmt::Debug;
+use tokio::task::JoinHandle;
+use typed_builder::TypedBuilder;
+
+/// [`DependentFailBehavior`] is a trait for implementing a behavior when dependencies aren't resolved
+/// in [`DependencyTaskFrame`]. It takes nothing and returns a result for the [`DependencyTaskFrame`] to
+/// return. By default, [`DependencyTaskFrame`] uses [`DependentSuccessOnFail`]
+#[async_trait]
+pub trait DependentFailBehavior: Send + Sync {
+    async fn execute(&self) -> Result<(), TaskError>;
+}
+
+/// When dependencies aren't resolved, return an error, more specifically
+/// the ``ChronologErrors::TaskDependenciesUnresolved`` error
+pub struct DependentFailureOnFail;
+
+#[async_trait]
+impl DependentFailBehavior for DependentFailureOnFail {
+    async fn execute(&self) -> Result<(), TaskError> {
+        Err(Arc::new(ChronologErrors::TaskDependenciesUnresolved))
+    }
+}
+
+/// When dependencies aren't resolved, return a `Ok(())`
+pub struct DependentSuccessOnFail;
+
+#[async_trait]
+impl DependentFailBehavior for DependentSuccessOnFail {
+    async fn execute(&self) -> Result<(), TaskError> {
+        Ok(())
+    }
+}
+
+#[derive(TypedBuilder)]
+#[builder(build_method(into = DependencyTaskFrame<T>))]
+pub struct DependencyTaskFrameConfig<T: TaskFrame> {
+    frame: T,
+    dependencies: Vec<Arc<dyn FrameDependency>>,
+
+    #[builder(
+        default = Arc::new(DependentFailureOnFail),
+        setter(transform = |ts: impl DependentFailBehavior + 'static| Arc::new(ts) as Arc<dyn DependentFailBehavior>)
+    )]
+    dependent_behaviour: Arc<dyn DependentFailBehavior>,
+}
+
+impl<T: TaskFrame> From<DependencyTaskFrameConfig<T>> for DependencyTaskFrame<T> {
+    fn from(config: DependencyTaskFrameConfig<T>) -> Self {
+        Self {
+            frame: config.frame,
+            dependencies: config.dependencies,
+            on_start: TaskEvent::new(),
+            on_end: TaskEvent::new(),
+            dependent_behaviour: config.dependent_behaviour,
+        }
+    }
+}
+
+/// Represents an **dependent task frame** that directly wraps a task frame and executes it only if
+/// all dependencies are resolved. This task frame type acts asa **wrapper node** within the task frame
+/// hierarchy. Allowing the creation of task frames that depend on other tasks, in addition to allowing
+/// dynamic execution (which opens the door for optimizations in case dependencies are expensive to compute)
+///
+/// # Events
+/// When it comes to events, [`DependencyTaskFrame`] comes with the default set of events, as
+/// there is nothing else to listen for / subscribe to
+///
+/// # Example
+/// ```ignore
+/// use chronolog_core::schedule::TaskScheduleInterval;
+/// use chronolog_core::scheduler::{Scheduler, CHRONOLOG_SCHEDULER};
+/// use chronolog_core::task::executionframe::ExecutionTaskFrame;
+/// use chronolog_core::task::Task;
+///
+/// let task_frame = ExecutionTaskFrame::new(
+///     |_metadata| async {
+///         println!("Hello from an execution task!");
+///         Ok(())
+///     }
+/// );
+///
+/// let task = Task::define(TaskScheduleInterval::from_secs(2), task_frame);
+/// CHRONOLOG_SCHEDULER.register(task).await;
+/// ```
+pub struct DependencyTaskFrame<T: TaskFrame> {
+    frame: T,
+    dependencies: Vec<Arc<dyn FrameDependency>>,
+    dependent_behaviour: Arc<dyn DependentFailBehavior>,
+    on_start: TaskStartEvent,
+    on_end: TaskEndEvent,
+}
+
+impl<T: TaskFrame> DependencyTaskFrame<T> {
+    pub fn builder() -> DependencyTaskFrameConfigBuilder<T> {
+        DependencyTaskFrameConfig::builder()
+    }
+}
+
+#[async_trait]
+impl<T: TaskFrame> TaskFrame for DependencyTaskFrame<T> {
+    async fn execute(
+        &self,
+        metadata: Arc<dyn TaskMetadata + Send + Sync>,
+        emitter: Arc<TaskEventEmitter>,
+    ) -> Result<(), TaskError> {
+        let mut handles: Vec<JoinHandle<bool>> = Vec::new();
+
+        for dep in &self.dependencies {
+            let dep = dep.clone();
+            handles.push(tokio::spawn(async move { dep.is_resolved().await }));
+        }
+
+        let mut is_resolved = true;
+        for handle in handles {
+            match handle.await {
+                Ok(result) => {
+                    if !result {
+                        is_resolved = false;
+                        break;
+                    }
+                }
+                Err(_) => {
+                    is_resolved = false;
+                    break;
+                }
+            }
+        }
+
+        if !is_resolved {
+            return self.dependent_behaviour.execute().await;
+        }
+
+        self.frame.execute(metadata.clone(), emitter).await
+    }
+
+    fn on_start(&self) -> TaskStartEvent {
+        self.on_start.clone()
+    }
+
+    fn on_end(&self) -> TaskEndEvent {
+        self.on_end.clone()
+    }
+}
