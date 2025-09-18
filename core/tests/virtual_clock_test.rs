deb868958f97a6319aad4d51ef0fbf758617d0c5
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-18 11:32:05 +0300

    Some slight adjustments

--- core/tests/virtual_clock_test.rs
+++ core/tests/virtual_clock_test.rs
@@ -1,3 +1,4 @@
+use std::time::Duration;
 use chronolog_core::clock::AdvanceableScheduleClock;
 use chronolog_core::clock::SchedulerClock;
 use chronolog_core::clock::VirtualClock;
@@ -13,6 +14,9 @@ macro_rules! assert_approx {
     }};
 }
 
+// A small value to avoid floating precision errors
+pub const EPSILON: Duration = Duration::from_millis(1);
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -21,14 +25,16 @@ mod tests {
     #[tokio::test]
     async fn test_initial_epoch() {
         let clock = VirtualClock::from_epoch();
-        assert_approx!(clock.now().await, UNIX_EPOCH, Duration::from_millis(1));
+        assert_approx!(clock.now().await, UNIX_EPOCH, EPSILON);
     }
+
     #[tokio::test]
     async fn test_custom_time() {
         let time0 = UNIX_EPOCH + Duration::from_secs(45);
         let clock = VirtualClock::new(time0);
-        assert_approx!(clock.now().await, time0, Duration::from_millis(1));
+        assert_approx!(clock.now().await, time0, EPSILON);
     }
+
     #[tokio::test]
     async fn test_advance() {
         let clock = VirtualClock::from_epoch();
@@ -37,12 +43,13 @@ mod tests {
         clock.advance(Duration::from_secs(100)).await;
         assert_eq!(clock.now().await, UNIX_EPOCH + Duration::from_secs(101));
     }
+
     #[tokio::test]
     async fn test_advance_to() {
         let clock = VirtualClock::from_epoch();
         let target = UNIX_EPOCH + Duration::from_secs(19);
         clock.advance_to(target).await;
-        assert_approx!(clock.now().await, target, Duration::from_millis(1));
+        assert_approx!(clock.now().await, target, EPSILON);
     }
 
     #[tokio::test]
@@ -52,6 +59,6 @@ mod tests {
         clock.advance(Duration::from_secs(5)).await;
         clock.idle_to(target).await;
         let now = clock.now().await;
-        assert_approx!(now, target, Duration::from_millis(1));
+        assert_approx!(now, target, EPSILON);
     }
 }
