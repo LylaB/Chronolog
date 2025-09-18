425581ca8c0f00889b589735a33889854f1b6710
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-18 17:24:15 +0300

    Added FlagDependency for resolving based on if an outside boolean value is true

---
+++ core/src/task/dependency/flag.rs
@@ -0,0 +1,68 @@
+use crate::task::Task;
+use crate::task::dependency::{
+    FrameDependency, ResolvableFrameDependency, UnresolvableFrameDependency,
+};
+use async_trait::async_trait;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
+
+/// [`FlagDependency`] is a dependency which can be enabled and disabled from outside, essentially
+/// acting as a checkbox. This dependency can be enabled/disabled and even developers can manually resolve
+/// it or manually unresolve it
+///
+/// # Example
+/// ```rust
+/// use std::sync::Arc;
+/// use std::sync::atomic::{AtomicBool, Ordering};
+/// use chronolog_core::task::dependency::{FlagDependency, FrameDependency};
+///
+/// let val = Arc::new(AtomicBool::new(false));
+///
+/// // Creating the dependency
+/// let flag = FlagDependency::new(val.clone());
+///
+/// // ... Some time passes after creation ...
+///
+/// val.store(true, Ordering::Relaxed);
+/// assert!(flag.is_resolved());
+/// ```
+pub struct FlagDependency(Arc<AtomicBool>, Arc<AtomicBool>);
+
+impl FlagDependency {
+    pub fn new(flag: Arc<AtomicBool>) -> Self {
+        Self(flag, Arc::new(AtomicBool::new(true)))
+    }
+}
+
+#[async_trait]
+impl FrameDependency for FlagDependency {
+    async fn is_resolved(&self) -> bool {
+        self.0.load(Ordering::Relaxed)
+    }
+
+    async fn disable(&self) {
+        self.1.store(false, Ordering::Relaxed);
+    }
+
+    async fn enable(&self) {
+        self.1.store(true, Ordering::Relaxed);
+    }
+
+    async fn is_enabled(&self) -> bool {
+        self.1.load(Ordering::Relaxed)
+    }
+}
+
+#[async_trait]
+impl ResolvableFrameDependency for FlagDependency {
+    async fn resolve(&self) {
+        self.0.store(true, Ordering::Relaxed);
+    }
+}
+
+#[async_trait]
+impl UnresolvableFrameDependency for FlagDependency {
+    async fn unresolve(&self) {
+        self.0.store(true, Ordering::Relaxed);
+    }
+}
