e9488d9acfd90ca18f2d8739f2a27655ec2a93d9
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-09-20 11:49:35 +0300

    Changed the argument types from SchedulerTaskStore and added an implementation for SchedulerTaskStore on Arc<TS> where TS is a SchedulerTaskStore

--- core/src/scheduler/task_store.rs
+++ core/src/scheduler/task_store.rs
@@ -27,11 +27,42 @@ pub trait SchedulerTaskStore: Send + Sync {
     async fn reschedule(&self, clock: Arc<dyn SchedulerClock>, task: Arc<Task>, idx: usize);
 
     /// Stores a task as an entry, returning its index
-    async fn store(&self, clock: Arc<dyn SchedulerClock>, task: Task) -> usize;
+    async fn store(&self, clock: Arc<dyn SchedulerClock>, task: Arc<Task>) -> usize;
 
     /// Removes a task based on an index
     async fn remove(&self, idx: usize);
 
     /// Clears fully all the contents of the task store
     async fn clear(&self);
+}
+
+#[async_trait]
+impl<TS: SchedulerTaskStore + 'static> SchedulerTaskStore for Arc<TS> {
+    async fn retrieve(&self) -> Option<(Arc<Task>, SystemTime, usize)> {
+        self.as_ref().retrieve().await
+    }
+
+    async fn pop(&self) {
+        self.as_ref().pop().await
+    }
+
+    async fn exists(&self, idx: usize) -> bool {
+        self.as_ref().exists(idx).await
+    }
+
+    async fn reschedule(&self, clock: Arc<dyn SchedulerClock>, task: Arc<Task>, idx: usize) {
+        self.as_ref().reschedule(clock, task, idx).await
+    }
+
+    async fn store(&self, clock: Arc<dyn SchedulerClock>, task: Arc<Task>) -> usize {
+        self.as_ref().store(clock, task).await
+    }
+
+    async fn remove(&self, idx: usize) {
+        self.as_ref().remove(idx).await
+    }
+
+    async fn clear(&self) {
+        self.as_ref().clear().await
+    }
 }
